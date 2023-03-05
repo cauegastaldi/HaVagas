5acac7268b3bcea7364bdbcb9919e930182c84f7
Caue <cauegastaldi@outlook.com>
2023-03-05 20:10:12 -0300

    chore: replaced checkboxes by spinner button in field 'formacao'

--- app/src/main/java/br/edu/ifsp/scl/ads/havagas/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/havagas/MainActivity.kt
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.view.View
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
 import android.widget.RadioButton
 import android.widget.Toast
+import androidx.core.view.get
 import androidx.core.view.isVisible
 import br.edu.ifsp.scl.ads.havagas.databinding.ActivityMainBinding
 
@@ -31,65 +34,38 @@ class MainActivity : AppCompatActivity() {
                 }
             }
 
-            fundamentalCb.setOnClickListener {
-                if (fundamentalLl.visibility == View.GONE)
-                    fundamentalLl.visibility = View.VISIBLE
-                else {
-                    fundamentalLl.visibility = View.GONE
-                    anoFormacaoFundamentalEt.setText("")
-                }
-            }
+            formacaoSp.onItemSelectedListener = object : OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
 
-            medioCb.setOnClickListener {
-                if (medioLl.visibility == View.GONE)
-                    medioLl.visibility = View.VISIBLE
-                else {
-                    medioLl.visibility = View.GONE
-                    anoFormacaoMedioEt.setText("")
+                    anoFormacaoFundamentalMedioEt.setText("")
+                    conclusaoGraduacaoEspecializacaoEt.setText("")
+                    instituicaoGraduacaoEspecializacaoEt.setText("")
+                    instituicaoMestradoDoutoradoEt.setText("")
+                    conclusaoMestradoDoutoradoEt.setText("")
+                    monografiaMestradoDoutoradoEt.setText("")
+                    orientadorMestradoDoutoradoEt.setText("")
+                    if (position == 0 || position == 1) {
+                        fundamentalMedioLl.visibility = View.VISIBLE
+                        graduacaoEspecializacaoLl.visibility = View.GONE
+                        mestradoDoutoradoLl.visibility = View.GONE
+                    } else if (position == 2 || position == 3) {
+                        graduacaoEspecializacaoLl.visibility = View.VISIBLE
+                        fundamentalMedioLl.visibility = View.GONE
+                        mestradoDoutoradoLl.visibility = View.GONE
+                    } else {
+                        mestradoDoutoradoLl.visibility = View.VISIBLE
+                        fundamentalMedioLl.visibility = View.GONE
+                        graduacaoEspecializacaoLl.visibility = View.GONE
+                    }
                 }
-            }
 
-            graduacaoCb.setOnClickListener {
-                if (graduacaoLl.visibility == View.GONE)
-                    graduacaoLl.visibility = View.VISIBLE
-                else {
-                    graduacaoLl.visibility = View.GONE
-                    instituicaoGraduacaoEt.setText("")
-                    conclusaoGraduacaoEt.setText("")
-                }
-            }
-
-            especializacaoCb.setOnClickListener {
-                if (especializacaoLl.visibility == View.GONE)
-                    especializacaoLl.visibility = View.VISIBLE
-                else {
-                    especializacaoLl.visibility = View.GONE
-                    instituicaoEspecializacaoEt.setText("")
-                    conclusaoEspecializacaoEt.setText("")
-                }
-            }
-
-            mestradoCb.setOnClickListener {
-                if (mestradoLl.visibility == View.GONE)
-                    mestradoLl.visibility = View.VISIBLE
-                else {
-                    mestradoLl.visibility = View.GONE
-                    instituicaoMestradoEt.setText("")
-                    conclusaoMestradoEt.setText("")
-                    monografiaMestradoEt.setText("")
-                    orientadorMestradoEt.setText("")
-                }
-            }
-
-            doutoradoCb.setOnClickListener {
-                if (doutoradoLl.visibility == View.GONE)
-                    doutoradoLl.visibility = View.VISIBLE
-                else {
-                    doutoradoLl.visibility = View.GONE
-                    instituicaoDoutoradoEt.setText("")
-                    conclusaoDoutoradoEt.setText("")
-                    monografiaDoutradoEt.setText("")
-                    orientadorDoutoradoEt.setText("")
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    TODO("Not yet implemented")
                 }
             }
 
@@ -108,44 +84,22 @@ class MainActivity : AppCompatActivity() {
                 dataNascimentoEt.setText("")
                 celularEt.setText("")
                 celularLl.visibility = View.GONE
-                if (fundamentalCb.isChecked) {
-                    fundamentalCb.isChecked = false
-                    fundamentalLl.visibility = View.GONE
-                    anoFormacaoFundamentalEt.setText("")
+                formacaoSp.setSelection(0)
+                fundamentalMedioLl.visibility = View.VISIBLE
+                if (formacaoSp.selectedItemPosition == 0 || formacaoSp.selectedItemPosition == 1) {
+                    anoFormacaoFundamentalMedioEt.setText("")
+                } else if (formacaoSp.selectedItemPosition == 2 || formacaoSp.selectedItemPosition == 3) {
+                    graduacaoEspecializacaoLl.visibility = View.GONE
+                    conclusaoGraduacaoEspecializacaoEt.setText("")
+                    instituicaoGraduacaoEspecializacaoEt.setText("")
+                } else {
+                    mestradoDoutoradoLl.visibility = View.GONE
+                    instituicaoMestradoDoutoradoEt.setText("")
+                    conclusaoMestradoDoutoradoEt.setText("")
+                    monografiaMestradoDoutoradoEt.setText("")
+                    orientadorMestradoDoutoradoEt.setText("")
                 }
-                if (medioCb.isChecked) {
-                    medioCb.isChecked = false
-                    medioLl.visibility = View.GONE
-                    anoFormacaoMedioEt.setText("")
-                }
-                if (graduacaoCb.isChecked){
-                    graduacaoCb.isChecked = false
-                    graduacaoLl.visibility = View.GONE
-                    instituicaoGraduacaoEt.setText("")
-                    conclusaoGraduacaoEt.setText("")
-                }
-                if (especializacaoCb.isChecked){
-                    especializacaoCb.isChecked = false
-                    especializacaoLl.visibility = View.GONE
-                    instituicaoEspecializacaoEt.setText("")
-                    conclusaoEspecializacaoEt.setText("")
-                }
-                if (mestradoCb.isChecked){
-                    mestradoCb.isChecked = false
-                    mestradoLl.visibility = View.GONE
-                    instituicaoMestradoEt.setText("")
-                    conclusaoMestradoEt.setText("")
-                    monografiaMestradoEt.setText("")
-                    orientadorMestradoEt.setText("")
-                }
-                if (doutoradoCb.isChecked){
-                    doutoradoCb.isChecked = false
-                    doutoradoLl.visibility = View.GONE
-                    instituicaoDoutoradoEt.setText("")
-                    conclusaoDoutoradoEt.setText("")
-                    monografiaDoutradoEt.setText("")
-                    orientadorDoutoradoEt.setText("")
-                }
+
                 vagasInteresseEt.setText("")
             }
         }
@@ -180,87 +134,37 @@ class MainActivity : AppCompatActivity() {
 
             var formacao : String = ""
             var informacoesFormacao : MutableList<String> = mutableListOf()
-            if (fundamentalCb.isChecked) {
-                formacao += "Fundamental"
-                if (anoFormacaoFundamentalEt.text.isNotEmpty())
-                    informacoesFormacao.add("Ano de formatura: ${anoFormacaoFundamentalEt.text}")
-                if (informacoesFormacao.isNotEmpty())
-                    formacao += " (${informacoesFormacao.joinToString()})"
-                formacao += "; "
+            formacao += formacaoSp.selectedItem.toString()
+            if (formacaoSp.selectedItemPosition == 0 || formacaoSp.selectedItemPosition == 1) {
+                if (anoFormacaoFundamentalMedioEt.text.isNotEmpty())
+                    informacoesFormacao.add("Ano de formação: ${anoFormacaoFundamentalMedioEt.text}")
+            } else if (formacaoSp.selectedItemPosition == 2 || formacaoSp.selectedItemPosition == 3) {
+                if (conclusaoGraduacaoEspecializacaoEt.text.isNotEmpty())
+                    informacoesFormacao.add("Ano de conclusão: ${conclusaoGraduacaoEspecializacaoEt.text}")
+                if (instituicaoGraduacaoEspecializacaoEt.text.isNotEmpty())
+                    informacoesFormacao.add("Instituição: ${instituicaoGraduacaoEspecializacaoEt.text}")
+            } else {
+                if (conclusaoMestradoDoutoradoEt.text.isNotEmpty())
+                    informacoesFormacao.add("Ano de conclusão: ${conclusaoMestradoDoutoradoEt.text}")
+                if (instituicaoMestradoDoutoradoEt.text.isNotEmpty())
+                    informacoesFormacao.add("Instituição: ${instituicaoMestradoDoutoradoEt.text}")
+                if (monografiaMestradoDoutoradoEt.text.isNotEmpty())
+                    informacoesFormacao.add("Título de monografia: ${monografiaMestradoDoutoradoEt.text}")
+                if (orientadorMestradoDoutoradoEt.text.isNotEmpty())
+                    informacoesFormacao.add("Orientador: ${orientadorMestradoDoutoradoEt.text}")
+            }
+
+            if (informacoesFormacao.isNotEmpty()) {
+                formacao += " (${informacoesFormacao.joinToString()})"
                 informacoesFormacao.clear()
             }
 
-            if (medioCb.isChecked) {
-                formacao += "Médio"
-                if (anoFormacaoMedioEt.text.isNotEmpty())
-                    informacoesFormacao.add("Ano de formatura: ${anoFormacaoMedioEt.text}")
-
-                if (informacoesFormacao.isNotEmpty())
-                    formacao += " (${informacoesFormacao.joinToString()})"
-                informacoesFormacao.clear()
-                formacao += "; "
-            }
-
-            if (graduacaoCb.isChecked) {
-                formacao += "Graduação"
-                if (conclusaoGraduacaoEt.text.isNotEmpty())
-                    informacoesFormacao.add("Ano de conclusão: ${conclusaoGraduacaoEt.text}")
-                if (instituicaoGraduacaoEt.text.isNotEmpty())
-                    informacoesFormacao.add("Instituição: ${instituicaoGraduacaoEt.text}")
-                if (informacoesFormacao.isNotEmpty())
-                    formacao += " (${informacoesFormacao.joinToString()})"
-                formacao += "; "
-                informacoesFormacao.clear()
-            }
-
-            if (especializacaoCb.isChecked) {
-                formacao += "Especialização"
-                if (conclusaoEspecializacaoEt.text.isNotEmpty())
-                    informacoesFormacao.add("Ano de conclusão: ${conclusaoEspecializacaoEt.text}")
-                if (instituicaoEspecializacaoEt.text.isNotEmpty())
-                    informacoesFormacao.add("Instituição: ${instituicaoEspecializacaoEt.text}")
-                if (informacoesFormacao.isNotEmpty())
-                    formacao += " (${informacoesFormacao.joinToString()})"
-                formacao += "; "
-                informacoesFormacao.clear()
-            }
-
-            if (mestradoCb.isChecked) {
-                formacao += "Mestrado"
-                if (conclusaoMestradoEt.text.isNotEmpty())
-                    informacoesFormacao.add("Ano de conclusão: ${conclusaoMestradoEt.text}")
-                if (instituicaoMestradoEt.text.isNotEmpty())
-                    informacoesFormacao.add("Instituição: ${instituicaoMestradoEt.text}")
-                if (monografiaMestradoEt.text.isNotEmpty())
-                    informacoesFormacao.add("Título de monografia: ${monografiaMestradoEt.text}")
-                if (orientadorMestradoEt.text.isNotEmpty())
-                    informacoesFormacao.add("Orientador: ${orientadorMestradoEt.text}")
-                if (informacoesFormacao.isNotEmpty())
-                    formacao += " (${informacoesFormacao.joinToString()})"
-                formacao += "; "
-                informacoesFormacao.clear()
-            }
-
-            if (doutoradoCb.isChecked) {
-                formacao += "Doutorado"
-                if (conclusaoDoutoradoEt.text.isNotEmpty())
-                    informacoesFormacao.add("Ano de conclusão: ${conclusaoDoutoradoEt.text}")
-                if (instituicaoDoutoradoEt.text.isNotEmpty())
-                    informacoesFormacao.add("Instituição: ${instituicaoDoutoradoEt.text}")
-                if (monografiaDoutradoEt.text.isNotEmpty())
-                    informacoesFormacao.add("Título de monografia: ${monografiaDoutradoEt.text}")
-                if (orientadorDoutoradoEt.text.isNotEmpty())
-                    informacoesFormacao.add("Orientador: ${orientadorDoutoradoEt.text}")
-                if (informacoesFormacao.isNotEmpty())
-                    formacao += " (${informacoesFormacao.joinToString()})"
-                formacao += "; "
-                informacoesFormacao.clear()
-            }
-
+            campos.add("Formação: $formacao")
+            /*
             if (formacao.isNotEmpty()) {
                 formacao = formacao.substring(0, formacao.lastIndexOf(';'))
                 campos.add("Formação: $formacao")
-            }
+            }*/
 
             if (vagasInteresseEt.text.isNotEmpty())
                 campos.add("Vagas de interesse: ${vagasInteresseEt.text}")
